a42edee5df1f9c18032ffc6c59202f29624cda19
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2023-12-03 18:36:21 +0530

    appwrite authentication service

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
-    <h1>hello</h1>
+      <h1>hello</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
