fb0ea94be6ae8881b911634b3b8aff92fa852daa
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2023-12-08 00:32:28 +0530

    Real time editor, signup form, post form

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,17 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
+import RTE from "./RTE";
+import Select from "./Select";
 
-
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  RTE,
+  Select,
+  Input,
+};
